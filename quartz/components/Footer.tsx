291ce22b2f22b1cbe6f739a44c943ff1e13ff3f0
이진서 <dev037@4csoft.com>
2025-09-11 18:09:34 +0900

    manual

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -2,11 +2,11 @@ import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } fro
 import style from "./styles/footer.scss"
 import { version } from "../../package.json"
 import { i18n } from "../i18n"
-
+ 
 interface Options {
   links: Record<string, string>
 }
-
+ 
 export default ((opts?: Options) => {
   const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
     const year = new Date().getFullYear()
@@ -14,13 +14,18 @@ export default ((opts?: Options) => {
     return (
       <footer class={`${displayClass ?? ""}`}>
         <p>
-          <a href="https://4csoft.com/">LX2</a>
+ 
+          <a href="https://lx2.4csoft.com/">
+            <img src="/_etc/lx2.png" alt="LX2 로고" height="100" />
+          </a>
         </p>
-    
+        <ul>
+        </ul>
       </footer>
     )
   }
-
+ 
   Footer.css = style
   return Footer
 }) satisfies QuartzComponentConstructor
+ 
